f9f2fc8f3367812091355b18598cc5c4a39e6e3b
PabloDev <yauripablo70@gmail.com>
2025-12-13 11:33:19 -0500

    Se agrego la carpeta de config y middlewares con sus acciones respectivas

---
+++ backend/src/middlewares/autenticacionMiddleware.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+
+const autenticacionToken = (req, res, next) => {
+  const autenticacionHeader = req.headers['authorization'];
+  const token = autenticacionHeader && autenticacionHeader.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ message: 'Token no proporcionado' });
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, usuario) => {
+    if (err) {
+      return res.status(403).json({ message: 'Token inválido o expirado' });
+    }
+    req.usuario = usuario;
+    next();
+  });
+};
+
+const verificarRoles = (...rolesPermitidos) => {
+  return (req, res, next) => {
+    const { idRol } = req.usuario;
+
+    if (!rolesPermitidos.includes(idRol)) {
+      return res.status(403).json({ message: 'Acceso denegado. Rol no autorizado.' });
+    }
+
+    next();
+  };
+};
+
+module.exports = {
+  autenticacionToken,
+  verificarRoles
+};
---
+++ backend/src/middlewares/limitador.js
@@ -0,0 +1,12 @@
+const rateLimit = require('express-rate-limit');
+
+const limitador = rateLimit({
+  windowMs: 1 * 60 * 1000, 
+  max: 25, 
+  message: {
+    ok: false,
+    mensaje: 'Demasiadas peticiones. Intenta de nuevo en 1 minuto.'
+  }
+});
+
+module.exports = limitador;
