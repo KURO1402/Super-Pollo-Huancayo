594ac41c3fc8565b837b50ee04d435fcf5ae7227
PabloDev <yauripablo70@gmail.com>
2026-02-05 20:39:12 -0500

    Creacion de carpetas para el modulo de reservacion

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,11 @@ const cors = require('cors');
 
 const app = express();
 
-/*const corsOptions = {
+const corsOptions = {
   origin: 'http://localhost:5173',
   credentials: true,
-};*/
-app.use(cors());
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
