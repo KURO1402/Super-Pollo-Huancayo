a8038bbb144f0158998cfb245bd06cd5cc636b87
PabloDev <yauripablo70@gmail.com>
2026-03-27 10:16:59 -0500

    Cambios de version

--- backend/src/config/conexion_DB.js
+++ backend/src/config/conexion_DB.js
@@ -3,16 +3,15 @@ const mysql = require('mysql2/promise');
 
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST, 
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  user: process.env.DB_USER, 
-  password: process.env.DB_PASSWORD, 
-  database: process.env.DB_NAME, 
-  waitForConnections: true, 
-  connectionLimit: 15, 
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 15,
   queueLimit: 0,
-  connectTimeout: 20000 
+  connectTimeout: 20000
 });
-
 
 module.exports = pool;
