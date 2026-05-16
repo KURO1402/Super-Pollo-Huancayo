ebd906b49069d42e0bf36bf749fa0f5364788eae
PabloDev <yauripablo70@gmail.com>
2026-05-16 13:47:53 -0500

    Se agrego una accion para correr el workflow en github sin la necesidad de hacer push o merge a main cuando no hay modificaciones de codigo solo configuraciones, ademas se descomento unas lineas de codigo que estaban descomentadas para pruebas

--- backend/src/middlewares/verificar_cliente_movil.js
+++ backend/src/middlewares/verificar_cliente_movil.js
@@ -2,7 +2,7 @@ const verificarClienteMovil = (req, res, next) => {
   const apiKey = req.headers['x-api-key'];
   const userAgent = req.headers['user-agent'] || '';
 
-  // Verificar API Key
+
   if (!apiKey) {
     return res.status(401).json({ ok: false, mensaje: 'Se requiere API Key' });
   }
@@ -11,10 +11,10 @@ const verificarClienteMovil = (req, res, next) => {
     return res.status(403).json({ ok: false, mensaje: 'API Key inválida' });
   }
 
-  /*const esNavegador = /Mozilla|Chrome|Safari|Firefox|Edge|Opera/i.test(userAgent);
+  const esNavegador = /Mozilla|Chrome|Safari|Firefox|Edge|Opera/i.test(userAgent);
   if (esNavegador) {
     return res.status(403).json({ ok: false, mensaje: 'Acceso no permitido' });
-  }*/
+  }
 
   next();
 };
