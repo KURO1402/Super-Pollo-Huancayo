0a6f4e65668745808601d2ae13853dd329a91cab
PabloDev <yauripablo70@gmail.com>
2026-08-18 16:55:13 -0500

    Se corrigio la ruta de descarga del apk del movil

--- frontend/src/componentes/panel-admin/DesplegableDescargaApp.jsx
+++ frontend/src/componentes/panel-admin/DesplegableDescargaApp.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 
-const URL_APK = `${import.meta.env.VITE_BACKEND_URL}/api/descargas/super_pollo.apk`;
+const URL_APK = `${import.meta.env.VITE_BACKEND_URL}/descargas/super_pollo.apk`;
 const VERSION_APP = "1.0.1";
 
 const DesplegableDescargaApp = () => {
